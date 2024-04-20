34322a2ceca75e2e411825ec0d715aa34faa6c28
rencewang <47406212+rencewang@users.noreply.github.com>
2024-04-20 16:02:12 -0700

    date format, style changes

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const options = { month: 'long', day: 'numeric', year: 'numeric' };
+  return date.toLocaleDateString('en-US', options);
+}
+
 function populateAudioInfoAndPlayer(filePath, trackData) {
   const audioInfo = document.getElementById('audio-info');
   const audioPlayer = document.getElementById('audio');
 
+  // Populate audio-info and audio with data
   audioPlayer.src = filePath;
+  audioInfo.innerHTML = `<div class="track-title">${trackData.title}</div>
+                         <div class="track-date">${formatDate(
+                           trackData.date
+                         )}</div>
+                         <div class="track-location">${
+                           trackData.location
+                         }</div>`;
 
-  // Populate audio-info section with metadata
-  audioInfo.innerHTML = `<div>${trackData.title}</div>
-                         <div>${trackData.date}</div>
-                         <div>${trackData.location}</div>`;
+  // Highlight the corresponding dot as active
+  const dots = document.querySelectorAll('.dot');
+  dots.forEach((dot) => dot.classList.remove('active-dot'));
+  const activeDot = document.querySelector(`.dot[data-filepath="${filePath}"]`);
+  activeDot.classList.add('active-dot');
 }
 
 const handleTrackClick = (filePath, trackData) => {
@@ -17,10 +32,10 @@ const handleTrackClick = (filePath, trackData) => {
 fetch('metadata.json')
   .then((response) => response.json())
   .then((metadata) => {
-    const audioList = document.getElementById('audio-list');
-
     // Iterate over metadata entries and populate audio list
     for (const filePath in metadata) {
+      const audioList = document.getElementById('audio-list');
+
       if (metadata.hasOwnProperty(filePath)) {
         const trackData = metadata[filePath];
 
@@ -41,5 +56,10 @@ fetch('metadata.json')
         audioList.appendChild(dot);
       }
     }
+
+    populateAudioInfoAndPlayer(
+      Object.keys(metadata)[0],
+      metadata[Object.keys(metadata)[0]]
+    );
   })
   .catch((error) => console.error('Error fetching metadata:', error));
